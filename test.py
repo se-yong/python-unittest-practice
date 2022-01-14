d5110ce7753e3876e6ab0bde94944998dfd46e4c
se-yong <parksae0428@gmail.com>
2022-01-14 18:14:30 +0900

    unittest 예제 테스트

---
+++ test.py
@@ -0,0 +1,24 @@
+import unittest
+import mycalc
+
+class MyCalcTest(unittest.TestCase):
+    def test_add(self):
+        c = mycalc.add(20,10)
+        self.assertEqual(c, 30)
+
+    def test_substract(self):
+        c = mycalc.substract(20, 10)
+        self.assertEqual(c, 10)
+    
+    def test_add_error(self):
+        c = mycalc.add(20,10)
+        self.assertEqual(c, 35)
+    
+    def test_substarct_error(self):
+        c = mycalc.substract(20, 10)
+        self.assertEqual(c, 15)
+
+if __name__ == '__main__':
+    unittest.main()
+
+# 터미널 테스트 실행 명령어 python -m unittest --v 
